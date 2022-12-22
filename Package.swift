bcb54d313a6ab16fc3f8fa5884ec0f5f331c6ae3
Huy Nguyen <conghuy2012@gmail.com>
2022-12-22 23:20:52 +0700

    Update Podspec tag and package iOS version

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "DataCache",
     platforms: [
         .macOS(.v10_11),
-        .iOS(.v9)
+        .iOS(.v13)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
